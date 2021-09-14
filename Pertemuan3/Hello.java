f08d30ac4e6686ce67b1716df6bfd00bb921a0e5
baskaraadi <baskaraadi.2021@student.uny.ac.id>
2021-09-14 13:15:48 +0700

    Pertemuan3

---
+++ Pertemuan3/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+/*
+Nama : Baskara Adi Nugraha
+NIM : 21537144011
+*/
+    public static void main(String[] args) {
+        // Untuk menampilkan teks
+        System.out.println("Hello world");
+    }
+}
