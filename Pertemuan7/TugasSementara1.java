9025182d92fca347f10fe0878c952c2a82a776dd
baskaraadi <baskaraadi.2021@student.uny.ac.id>
2021-10-21 21:44:49 +0700

    Pertemuan7

---
+++ Pertemuan7/TugasSementara1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class TugasSementara1 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukan nilai n = ");
+        int n = scan.nextInt();
+        for (int i = 1; i<=n; i++) {
+            for (int j = 1; j<=i; j++) {
+                if (i % 2 == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("#");
+                } 
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ Pertemuan7/TugasSementara2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class TugasSementara2 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukan nilai n = ");
+        int n = scan.nextInt();
+        for (int i = 1; i<=n; i++) {
+            for (int j = 1; j<=i; j++) {
+                if (i % 2 == 0) {
+                    System.out.print("*");
+                } else {
+                    if (j % 2 == 0) {
+                        System.out.print("-");     
+                    } else {
+                        System.out.print("#");
+                    }
+                } 
+            }
+            System.out.println("");
+        }
+    }
+}
